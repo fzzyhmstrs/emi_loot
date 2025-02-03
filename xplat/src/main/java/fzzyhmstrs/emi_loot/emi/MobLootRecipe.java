8be0183b3eed77a7bfff487683db85e13af6adf3
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-02-03 12:14:56 -0500

    0.7.5 fixes for 1.20.6

--- xplat/src/main/java/fzzyhmstrs/emi_loot/emi/MobLootRecipe.java
+++ xplat/src/main/java/fzzyhmstrs/emi_loot/emi/MobLootRecipe.java
@@ -99,7 +99,7 @@ public class MobLootRecipe implements EmiRecipe {
                 try {
                     addWidgetBuilders(builtPool, false);
                 } catch (Throwable e) {
-                    EmiLoot.LOGGER.error("Error encountered while preparing layout for mob recipe {}, display may be incomplete.", loot.id);
+                    EMILoot.LOGGER.error("Error encountered while preparing layout for mob recipe {}, display may be incomplete.", loot.id);
                     e.printStackTrace();
                 }
             }
