a3101a5ecc61b840b971f10db01e7a55ca4e113b
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-02-03 12:45:39 -0500

    0.7.5 fixes for 1.20.4

--- xplat/src/main/java/fzzyhmstrs/emi_loot/client/ClientChestLootTable.java
+++ xplat/src/main/java/fzzyhmstrs/emi_loot/client/ClientChestLootTable.java
@@ -6,6 +6,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 public class ClientChestLootTable implements LootReceiver {
 
@@ -34,7 +35,7 @@ public class ClientChestLootTable implements LootReceiver {
     }
 
     @Override
-    public LootReceiver fromBuf(PacketByteBuf buf) {
+    public LootReceiver fromBuf(PacketByteBuf buf, World world) {
         Identifier id = AbstractTextKeyParsingClientLootTable.getIdFromBuf(buf);
         int mapCount = buf.readShort();
         Object2FloatMap<ItemStack> itemMap = new Object2FloatOpenHashMap<>();
