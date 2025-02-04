83924eb3a5cb690951107549c72c68a53e74fdb4
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-02-03 19:02:25 -0500

    0.7.5 get world join successful again

--- xplat/src/main/java/fzzyhmstrs/emi_loot/client/ClientLootTablesReceiver.java
+++ xplat/src/main/java/fzzyhmstrs/emi_loot/client/ClientLootTablesReceiver.java
@@ -6,23 +6,23 @@ import me.fzzyhmstrs.fzzy_config.networking.api.ClientPlayNetworkContext;
 public class ClientLootTablesReceiver {
 
 	public static void receiveChestSender(SimpleFzzyPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveChestSender(payload.buf(), ctx.player().getWorld());
+		ClientLootTables.INSTANCE.receiveChestSender(payload.buf());
 	}
 
 	public static void receiveBlockSender(SimpleFzzyPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveBlockSender(payload.buf(), ctx.player().getWorld());
+		ClientLootTables.INSTANCE.receiveBlockSender(payload.buf());
 	}
 
 	public static void receiveMobSender(SimpleFzzyPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveMobSender(payload.buf(), ctx.player().getWorld());
+		ClientLootTables.INSTANCE.receiveMobSender(payload.buf());
 	}
 
 	public static void receiveGameplaySender(SimpleFzzyPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveGameplaySender(payload.buf(), ctx.player().getWorld());
+		ClientLootTables.INSTANCE.receiveGameplaySender(payload.buf());
 	}
 
 	public static void receiveArchaeologySender(SimpleFzzyPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveArchaeologySender(payload.buf(), ctx.player().getWorld());
+		ClientLootTables.INSTANCE.receiveArchaeologySender(payload.buf());
 	}
 
 }
